039257d53ec7d5b9f6ccc1c93c222755b3ed581d
Gustavo <gustavobenn@hotmail.com>
2017-10-06 09:18:10 -0300

    Update libass and example

--- example-videojs/videojs.libass-onecanvas.js
+++ example-videojs/videojs.libass-onecanvas.js
@@ -1,76 +1,26 @@
-(function (videojs, libjass) {
-    'use strict';
-    
-    var vjs_ass = function (options) {
-        var overlay = document.createElement('div'),
-            player  = this,
-            clock   = null,
-            lib     = null,
-            render  = null,
-            track   = null,
-            clockRate = options.rate || 1,
-            delay   = options.delay || 0,
-            OverlayComponent = null,
-            ready   = false;
-        
-        if (!options.src) {
+(function (videojs) {
+	'use strict';
+	
+	var libassjs = function (options) {
+		var player = this,
+			overlay = document.createElement('div'),
+			OverlayComponent = null,
+			canvas = document.createElement('canvas'),
+			ctx = canvas.getContext('2d'),
+			lib = null,
+			render = null,
+			track = null,
+			clock = null;
+		
+		if (!options.src) {
             return;
         }
-        
-        lib = ass_library_init();
+		
+		lib = ass_library_init();
         render = ass_renderer_init(lib);
-        
-        function getVideoWidth() {
-            return options.videoWidth || player.videoWidth() || player.el().offsetWidth;
-        }
-        
-        function getVideoHeight() {
-            return options.videoHeight || player.videoHeight() || player.el().offsetHeight
-        }
-        
-        ass_set_frame_size(render, getVideoWidth(), getVideoHeight());
-        console.log(getVideoWidth() + 'x' + getVideoHeight());
-        
-        function loadFont(url, fontName) {
-            var fn = url.substring(url.lastIndexOf('/')+1);
-            
-            return new Promise(function (resolve, reject) {
-                var xhr = new XMLHttpRequest();
-                xhr.open('GET', url, true);
-                xhr.responseType = 'arraybuffer';
-
-                xhr.onload = function(e) {
-                    var responseArray = new Uint8Array(this.response);
-                    console.log('Font ' + fn + ' loaded');
-                    ass_add_font(lib, fontName, responseArray, responseArray.length);
-                    resolve();
-                };
-
-                xhr.send();
-            });
-        }
-        
-        var fonts = [loadFont('LiberationSans-Bold.ttf', 'Liberation Sans Bold')];
-        
-        if (options.fonts) {
-            for (var i in options.fonts) {
-                fonts.push(loadFont(options.fonts[i][0], options.fonts[i][1]));
-            }
-        }
-            
-        Promise.all(fonts).then(function () {
-            ass_set_fonts(render, '', 'Liberation Sans Bold', 0, null, 0);
-            if (options.fonts_id) {
-                $('#'+options.fonts_id).hide();
-            }
-        });
-        
-        $.get( options.src, function(data) {
-            track = ass_read_memory(lib, data, data.length+1, null);
-        });
-        
-        overlay.className = 'vjs-ass';
-        
+		//ass_set_cache_limits(render, 1, 10);
+		
+		overlay.className = 'libassjs';
         OverlayComponent = {
             name: function () {
                 return 'AssOverlay'
@@ -79,15 +29,79 @@
                 return overlay;
             }
         };
-
-        function getCurrentTime() {
-            return player.currentTime() - delay;
+		
+		player.addChild(OverlayComponent);
+		
+		function getCurrentTime() {
+            return player.currentTime();
         }
-        
-        clock = new libjass.renderers.AutoClock(getCurrentTime, 500);
-        var changed = Module._malloc(4);
-        
-        function blend(dst, img, dst_w, dst_h) {
+		
+		function getVideoWidth() {
+            return parseInt(getComputedStyle(player.el()).width, 10);
+        }
+		
+		function getVideoHeight() {
+            return parseInt(getComputedStyle(player.el()).height, 10);
+        }
+		
+		function loadFromUrl(url, responseType='arraybuffer') {
+			return new Promise(function (resolve, reject) {
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', url, true);
+                xhr.responseType = responseType;
+
+                xhr.onload = function(e) {
+                    var responseArray = new Uint8Array(this.response);
+                    resolve(responseArray);
+                };
+				
+                xhr.send();
+            });
+		}
+		
+		function loadFont(url, fontName) {
+            var fn = url.substring(url.lastIndexOf('/')+1);
+			
+			return loadFromUrl(url).then(function (responseArray) {
+				console.log('Font ' + fn + ' loaded');
+                ass_add_font(lib, fontName, responseArray, responseArray.length);
+			});
+        }
+		
+		function loadFontsAndScript(fontsUrl, scriptUrl) {
+			return new Promise(function (resolve, reject) {
+				var fonts = [loadFont('LiberationSans-Bold.ttf', 'Liberation Sans Bold')];
+				for (var i in fontsUrl) {
+					fonts.push(loadFont(fontsUrl[i][0], fontsUrl[i][1]));
+				}
+				
+				Promise.all(fonts).then(function () {
+					ass_set_fonts(render, '', 'Liberation Sans Bold', 0, null, 0);
+					
+					loadFromUrl(scriptUrl).then(function (responseArray) {
+						console.log('Script ' + scriptUrl + ' loaded');
+						var decoder = new TextDecoder('utf8');
+						track = ass_read_memory(lib, decoder.decode(responseArray), responseArray.length, null);
+						resolve();
+					});
+				});
+			});
+		}
+		
+		function updateDisplayArea() {
+			setTimeout(function () {
+				var scaleX = getVideoWidth() / canvas.width;
+				var scaleY = getVideoHeight() / canvas.height;
+				var scaleToFit = Math.min(scaleX, scaleY);
+				
+				canvas.style.transformOrigin = '0 0';
+                canvas.style.transform = 'scale(' + scaleToFit + ')';
+                canvas.style.top = ((getVideoHeight() - (canvas.height * scaleToFit)) / 2) + 'px';
+				canvas.style.left = ((getVideoWidth() - (canvas.width * scaleToFit)) / 2) + 'px';
+			});
+		}
+		
+		function blend(dst, img, dst_w, dst_h) {
             var w = img.w,
                 h = img.h,
                 color = img.color,
@@ -121,13 +135,6 @@
             }
         }
         
-        function inside(x1, y1, w1, h1, x2, y2, w2, h2) {
-            if (x1 <= x2 && y1 <= y2 && h1 >= h2 && w1 >= w2) {
-                return true;
-            }
-            return false;
-        }
-        
         function clearRect(dst, dst_x, dst_y, w, h, dst_w, dst_h) {
             var dst_pos = (dst_y * dst_w * 4) + (dst_x * 4);
             
@@ -142,40 +149,54 @@
                 dst_pos += ((dst_w - x) * 4);
             }
         }
-        
-        var canvas = document.createElement('canvas');
-        var ctx = canvas.getContext('2d');
-        canvas.width = getVideoWidth();
-        canvas.height = getVideoHeight();
-        canvas.style.position = 'absolute';
-        overlay.appendChild(canvas);
-        
-        var clear = [];
+		
+		function overlap(x1, y1, w1, h1, x2, y2, w2, h2) {
+			var x_overlap = Math.max(0, Math.min(x1+w1, x2+w2) - Math.max(x1, x2));
+			var y_overlap = Math.max(0, Math.min(y1+h1, y2+h2) - Math.max(y1, y2));
+			return x_overlap != 0 || y_overlap != 0;
+		}
+		
+		function inside(x1, y1, w1, h1, x2, y2, w2, h2) {
+			if (x1 > x2 && x1+w1 < x2+w2 && y1 > y2 && y1+h1 < y2+h2) {
+				return true;
+			}
+			return false;
+		}
+		
+		var clear = [];
         var lastTime = 0;
-        var dst = new Uint8ClampedArray(canvas.width * canvas.height * 4);
-
-        function onClockTick() {
+        var dst = null;
+		var changed = Module._malloc(4);
+		
+		function onClockTick() {
+			if (!track) {
+				return;
+			}
+			
+			if (!dst) {
+				dst = new Uint8ClampedArray(canvas.width * canvas.height * 4);
+			}
+			
             var currentTime = clock.currentTime;
             
             if (lastTime > currentTime) {
                 return;
             }
             
+            var width = canvas.width;
+            var height = canvas.height;
+            
             lastTime = currentTime;
             var currentTimeLong = dcodeIO.Long.fromInt(parseInt(currentTime * 1000));
             var returnRenderFrame = ass_render_frame(render, track, currentTimeLong.low, currentTimeLong.high, changed);
             
-            var width = canvas.width;
-            var height = canvas.height;
-
-            if (changed != 0) {
-                
-                //pretty sure that there is better ways to do this.
+            if (Module.HEAPU8[changed] != 0) {
                 while(clear.length > 0) {
                     var c = clear.shift();
                     var i = false;
                     
-                    for (var c2 in clear) {
+                    for (var j in clear) {
+                        var c2 = clear[j];
                         if (inside(c[0], c[1], c[2], c[3], c2[0], c2[1], c2[2], c2[3])) {
                             i = true;
                             break;
@@ -186,80 +207,72 @@
                         clearRect(dst, c[0], c[1], c[2], c[3], width, height);
                     }
                 }
-
+                
                 if(returnRenderFrame != 0) {
                     var img = new ASS_Image(returnRenderFrame);
-                                        
+                    
                     while (img.valid) {
+                        if (!img.w || !img.h) {
+                            img = new ASS_Image(img.next);
+                            continue;
+                        }
+                        
                         blend(dst, img, width, height);
-                        
                         clear.push([img.dst_x, img.dst_y, img.w, img.h]);
-                        
                         img = new ASS_Image(img.next);
                     }
-                    
-                    ctx.putImageData(new ImageData(dst, width, height), 0, 0);
                 }
+                
+                ctx.putImageData(new ImageData(dst, width, height), 0, 0);
             }
-        };
-        
-        clock.addEventListener(libjass.renderers.ClockEvent.Tick, onClockTick);
-        
-        player.addChild(OverlayComponent);
-        
-        player.on('play', function () {
-            clock.play();
-        });
-        
-        player.on('pause', function () {
-            clock.pause();
-        });
-        
-        player.on('seeking', function () {
-            clock.seeking();
-            lastTime = 0;
-        });
-        
-        function updateClockRate() {
-            clock.setRate(player.playbackRate() * clockRate);
         }
-
-        updateClockRate();
-        player.on('ratechange', updateClockRate);
-        
-        function updateDisplayArea() {
-            setTimeout(function () {
-                console.log(getVideoWidth(), getVideoHeight());
-
-                var scaleX = getVideoWidth() / canvas.width;
-                var scaleY = getVideoHeight() / canvas.height;
-                
-                var scaleToFit = Math.min(scaleX, scaleY);
-                
-                canvas.style.transformOrigin = '0 0';
-                canvas.style.transform = 'scale(' + scaleToFit + ')';
-                canvas.style.top = ((getVideoHeight() - (canvas.height * scaleToFit)) / 2) + 'px';
-
-                //ass_set_frame_size(render, getVideoWidth(), getVideoHeight());
-                //canvas.width = getVideoWidth();
-                //canvas.height = getVideoHeight();
-                //dst = new Uint8ClampedArray(canvas.width * canvas.height * 4);
-            }, 100);
-        }
-        
-        window.addEventListener('resize', updateDisplayArea);
-        player.on('loadedmetadata', updateDisplayArea);
+		
+		loadFontsAndScript(options.fonts, options.src).then(function () {
+			if (options.fonts_id) {
+				for (var id in options.fonts_id) {
+					$('#'+options.fonts_id[id]).toggle();
+				}
+			}
+		});
+		
+		clock = new libjass.renderers.AutoClock(getCurrentTime, 500);
+		clock.addEventListener(libjass.renderers.ClockEvent.Tick, onClockTick);
+		
+		player.on('play', function () {
+			clock.play();
+		});
+		
+		player.on('pause', function () {
+			clock.pause();
+        });
+		
+		player.on('seeking', function () {
+			clock.seeking();
+        });
+		
+		player.on('ratechange', function () {
+			clock.setRate(player.playbackRate());
+		});
+		
+		player.on('loadedmetadata', updateDisplayArea);
         player.on('resize', updateDisplayArea);
         player.on('fullscreenchange', updateDisplayArea);
-        
-        player.on('dispose', function () {
-            clock.disable();
+		
+		player.on('dispose', function () {
+			clock.disable();
+			dst = null;
         });
         
         player.ready(function () {
-            
+            ass_set_frame_size(render, getVideoWidth(), getVideoHeight());
+			console.log(getVideoWidth() + 'x' + getVideoHeight());
+			
+			canvas.width = getVideoWidth();
+			canvas.height = getVideoHeight();
+			canvas.style.position = 'absolute';
+			overlay.appendChild(canvas);
         });
-    }
-    
-    videojs.plugin('ass', vjs_ass);
-}(window.videojs, window.libjass));
+	}
+	
+	videojs.plugin('ass', libassjs);
+}(window.videojs));
